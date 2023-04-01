898e6abb327aa8337e2bfb9b5bd8465fa1b2f726
User <User@LAPTOP-UIF87597>
2023-04-01 23:25:58 +0700

    edit code #1

--- Tower of Hanoi/Program.cs
+++ Tower of Hanoi/Program.cs
@@ -7,13 +7,13 @@ namespace Tower_of_Hanoi
         static void hanoi(int n , char a , char b , char c)
         {
             if(n == 0) { return; }
-            hanoi(n - 1, a, c, b);
-            Console.WriteLine("Disc "+n+" Form "+ a +" To "+ c);
-            hanoi(n - 1, b, c, a);
+            hanoi(n - 1,a,c,b);
+            Console.WriteLine("Disc "+n+" From "+a+" To "+c);
+            hanoi(n - 1, b, a, c);
         }
         static void Main(string[] args)
         {
-            int disc = 2;
+            int disc = 3;
             char start = 'A';
             char mid = 'B';
             char end = 'C';
