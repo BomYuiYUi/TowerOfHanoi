4aed6519e87aa6d40abd8bd489f4ebd223b36d1d
User <User@LAPTOP-UIF87597>
2023-04-01 22:56:35 +0700

    Add project files.

---
+++ Tower of Hanoi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tower_of_Hanoi
+{
+    class Program
+    {
+        static void hanoi(int n , char a , char b , char c)
+        {
+            if(n == 0) { return; }
+            hanoi(n - 1, a, c, b);
+            Console.WriteLine("Disc "+n+" Form "+ a +" To "+ c);
+            hanoi(n - 1, b, c, a);
+        }
+        static void Main(string[] args)
+        {
+            int disc = 2;
+            char start = 'A';
+            char mid = 'B';
+            char end = 'C';
+            hanoi(disc, start, mid, end);
+        }
+    }
+}
